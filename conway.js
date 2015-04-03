1ec65048b1f4f4aaf612e9d687b870ccb3ee39f6
Hannah <hunter.hannah@icloud.com>
2015-04-02 22:41:52 -0500

    fixed cell click selection

--- conway.js
+++ conway.js
@@ -7,27 +7,33 @@ function Cell(h, w){
       this.x = w;
       this.isAlive = false;
       this.updateLife = function() {
-            console.log("hi from the cell!")
             if(this.isAlive){
                   this.$td.addClass("alive");
             } else {
                   this.$td.removeClass("alive");
             };
       };
+      this.setupOnclick = function() {
+            var cell = this;
+            cell.$td.click(function(){
+                  cell.isAlive = !this.isAlive;
+                  cell.updateLife();
+            });
+      };
 };
 
 // Create data/DOM structure of grid - Push cell structure to array
 function generateGrid(height, width){
-      for(var h = 0; h <= height; h++){
+      for(var h = 0; h < height; h++){
             var $tr = $("<tr>");
             $(".game-grid").append($tr);
             grid[h] =  [];
-            for(var w = 0; w <= width; w++){
+            for(var w = 0; w < width; w++){
                 var cell = new Cell(h, w);
                 var $td = $("<td>");
                 $tr.append($td);
                 cell.$td = $td;
-                grid[h].push(cell);
+                grid[h][w] = cell;
             };
       };
 };
@@ -35,13 +41,7 @@ function generateGrid(height, width){
 function runGrid(height, width){
       for(var h = 0; h < height; h++){
             for(var w = 0; w < width; w++){
-                  var cell = grid[h][w]
-                  cell.$td.click(function(){
-                        cell.isAlive = !this.isAlive;
-                        cell.updateLife;
-                        console.log(cell);
-                        // $(this).addClass("alive");
-                  });
+                  grid[h][w].setupOnclick();
             };
       };
 };
