ac5d7e7add163487a09ee13d96433c270e0348b4
Hannah <hunter.hannah@icloud.com>
2015-04-04 03:50:46 -0500

    cleaned up code style

--- conway.js
+++ conway.js
@@ -3,23 +3,23 @@ var height = 25;
 var width = 50;
 
 function Cell(h, w){
-      this.y = h;
-      this.x = w;
-      this.isAlive = false;
-      this.updateLife = function() {
-            if(this.isAlive){
-                  this.$td.addClass("alive");
-            } else {
-                  this.$td.removeClass("alive");
-            };
-      };
-      this.setupOnclick = function() {
-            var cell = this;
-            cell.$td.click(function(){
-                  cell.isAlive = !this.isAlive;
-                  cell.updateLife();
-            });
-      };
+    this.y = h;
+    this.x = w;
+    this.isAlive = false;
+    this.updateLife = function() {
+        if(this.isAlive){
+            this.$td.addClass("alive");
+        } else {
+            this.$td.removeClass("alive");
+        };
+    };
+    this.setupOnclick = function() {
+        var cell = this;
+        cell.$td.click(function(){
+        cell.isAlive = !this.isAlive;
+        cell.updateLife();
+        });
+    };
 };
 
 function createCell(h, w){
@@ -31,17 +31,17 @@ function createCell(h, w){
 };
 
 function gridBus(height, width){
-      for(var h = 0; h < height; h++){
-            var $tr = $("<tr>");
-            $(".game-grid").append($tr);
-            grid[h] =  [];
-            for(var w = 0; w < width; w++){
-                $tr.append(createCell(h, w));
-                grid[h][w].setupOnclick();
-            };
-      };
+    for(var h = 0; h < height; h++){
+        var $tr = $("<tr>");
+        $(".game-grid").append($tr);
+        grid[h] =  [];
+        for(var w = 0; w < width; w++){
+            $tr.append(createCell(h, w));
+            grid[h][w].setupOnclick();
+        };
+    };
 };
 
 $(document).ready(function(){
     gridBus(height, width);
-   });
+});
