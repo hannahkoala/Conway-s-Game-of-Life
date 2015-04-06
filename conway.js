6a19096b8e12701abd288a11659e4990805c9247
Hannah <hunter.hannah@icloud.com>
2015-04-05 19:29:26 -0500

    working on neighbor detection

--- conway.js
+++ conway.js
@@ -1,6 +1,6 @@
 var grid = [];
-var height = 25;
-var width = 50;
+var height = 10;
+var width = 10;
 
 function Cell(h, w){
     this.y = h;
@@ -20,6 +20,55 @@ function Cell(h, w){
         cell.updateLife();
         });
     };
+    this.findNeighbors = function() {
+        this.neighbors = [];
+        for(var i = -1; i <= 1; i ++) {
+            var h1 = this.y + i;
+            if (h1 < 0) {
+                h1 += height;
+            };
+            if (h1 >= height) {
+                h1 -= height;
+            };
+            var row = grid[h1];
+            for(var j = -1; j <= 1; j ++) {
+                var w1 = this.x + j;
+                if (w1 < 0) {
+                    w1 += width;
+                };
+                if (w1 >= width) {
+                    w1 -= height;
+                };
+                var cell = row[w1];
+                if(cell != this) {
+                    this.neighbors.push(cell);
+                }
+            }
+        }
+    };
+    this.countNeighbors = function(){
+        this.aliveNeighbors = 0;
+        for (var i = 0; i < this.neighbors.length; i ++) {
+            var neighbor = this.neighbors[i];
+            if (neighbor.alive) {
+                this.aliveNeighbors ++;
+            };
+        };
+    };
+    this.changeLifeState = function() {
+        if(this.alive) {
+            if (this.aliveNeighbors < 2) {
+                this.alive = false;
+                this.updateLife();
+            } else if (this.aliveNeighbors > 3) {
+                this.alive = false;
+                this.updateLife();
+            }
+        } else if (this.aliveNeighbors == 3) {
+            this.alive = true;
+            this.updateLife();
+        }
+    };
 };
 
 function createCell(h, w){
@@ -30,18 +79,30 @@ function createCell(h, w){
     return cell.$td;
 };
 
-function gridBus(height, width){
+function createDom(height, width){
     for(var h = 0; h < height; h++){
         var $tr = $("<tr>");
         $(".game-grid").append($tr);
         grid[h] =  [];
         for(var w = 0; w < width; w++){
             $tr.append(createCell(h, w));
-            grid[h][w].setupOnclick();
+        };
+    };
+};
+
+function runGrid(height, width){
+    for(var h = 0; h < height; h++){
+        for(var w = 0; w < width; w++){
+            var cell = grid[h][w];
+            cell.setupOnclick();
+            cell.findNeighbors();
+            cell.countNeighbors();
+            cell.changeLifeState();
         };
     };
 };
 
 $(document).ready(function(){
-    gridBus(height, width);
+    createDom(height, width);
+    setInterval(runGrid(height, width), 100)
 });
